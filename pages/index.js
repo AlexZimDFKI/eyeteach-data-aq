6985a7648e036e4cc2752e4db465bc0f9b9ca5fc
alex <>
2025-12-06 22:50:37 +0100

    init test

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+import Image from "next/image"
+
 export default function Home() {
   return (
     <div className="container">
@@ -11,10 +13,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Image src="/logo.svg"
+          width={250}
+          height={250}
+        />
+        <Header title="Hier ensteht ein EYE-TEACH Projekt" />
       </main>
 
       <Footer />
